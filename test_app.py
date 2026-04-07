a0397e123868528060dd065a8e542964c7707a2d
Aileen2003 <162086201+Aileen2003@users.noreply.github.com>
2026-04-07 10:03:38 -0600

    Fix test for home route message

--- test_app.py
+++ test_app.py
@@ -4,8 +4,8 @@ def test_home():
     cliente = app.test_client()
     respuesta = cliente.get("/")
     assert respuesta.status_code == 200
-    assert b"API funcionando en Flask" in respuesta.data
-
+    assert b"API funcionando en Railway" in respuesta.data
+    
 def test_saludo():
     cliente = app.test_client()
     respuesta = cliente.get("/saludo")
